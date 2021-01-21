e023ecdbed533a8002b8396124d1b851bdc29940
ibad nurhamim <nurhamimibad96@gmail.com>
2021-01-21 11:43:34 +0700

    [TUTORIAL] Materi Belajar Java Database -> Driver

---
+++ src/test/java/com/tutorial/database/DriverTest.java
@@ -0,0 +1,22 @@
+package com.tutorial.database;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DriverTest {
+
+    @Test
+    void testRegister() {
+
+        try {
+            Driver mysqlDriver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(mysqlDriver);
+        } catch (SQLException exception) {
+            fail(exception);
+        }
+    }
+}
